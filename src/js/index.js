3350c90ca634548acc908c3efe6193416454d096
Pablo Martín Anaquín <anaquinpm@gmail.com>
2021-07-05 12:56:25 -0300

    chore: JS counter running

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,50 @@
 document.addEventListener('DOMContentLoaded', () => {
-  // TODO: create and save the date 14days before start the count 
-  // * input and save in to storage the event day
-  // Date.UTC para poder cargar con numeros el día del evento -> mes del 0-11
+  // TODO: input and save in to storage the event day
   const trunc = (x) => Math.trunc(diffDate / x);
   const sustMillis = (s) => { diffDate = diffDate % s };
   const positiveNumber = (x) => x > 0;
-
-  function timer(msec) {
-    let intTime = trunc(msec);
-    sustMillis(msec);
+  
+  function timer(milliseconds) {
+    let intTime = trunc(milliseconds);
+    sustMillis(milliseconds);
     return intTime;
   }
-
-  // TODO: cuando llega a todo con "0" vuelve todos los digitos a 59 -> verificar que cuando sea el utlimo ciclo pare de contar.
-  function countDown() {
-    let index = remainingTime.length - 1;
+  
+  function sustNextDigit(index) {
+    let gratherIndex = remainingTime.findIndex(positiveNumber);
     if (gratherIndex != -1) {
-      if (remainingTime[index] > 0) {
-        remainingTime[index] = remainingTime[index] - 1;
-      } else {
-        for (index -= 1; remainingTime[index] === 0; index--) {
-          remainingTime[index] = 59;
-        }
-        (remainingTime[index] > 0) ? remainingTime[index] -= 1 : '';
-        remainingTime[3] = 59;
+      for (index -= 1; remainingTime[index] === 0; index--) {
+        remainingTime[index] = 59;
       }
+      remainingTime[index] -= 1;
+      remainingTime[indexSeg] = 59;
+    } else {
+      clearInterval(idInterval);
     }
+  }
+  
+  function countDown() {
+    (remainingTime[indexSeg] > 0) ? remainingTime[indexSeg] -= 1 : sustNextDigit(indexSeg);
+    
     console.log(`${remainingTime[0]} ${remainingTime[1]} ${remainingTime[2]} ${remainingTime[3]}`);
   }
+  
+  // Seteo manual de tiempo para el contador
+  // !Date.UTC para poder cargar con numeros el día del evento -> mes del 0-11
+  // const eventDay = new Date(Date.UTC(2021, 6, 6, 0, 58, 10));  //para seteo manual
 
-  const eventDay = new Date(Date.UTC(2021, 6, 4, 0, 58, 10));
+  // * Pruebas de tiempo
+  const eventDay = new Date();
+  // eventDay.setTime(eventDay.getTime() + 8.64e7*14 + 1000*3)  // 14d:0h:0m:3s
+  // eventDay.setTime(eventDay.getTime() + 3.6e6*1 + 1000*3)    // 1h:0m:3s
+  eventDay.setTime(eventDay.getTime() + 1000 * 3)            // 3seg
+
   const now = new Date();
 
+  let idInterval;
   let diffDate = eventDay - now;
   let remainingTime = [timer(8.64e7), timer(3.6e6), timer(6e4), timer(1000)]
-  let gratherIndex = remainingTime.findIndex(positiveNumber);
+  let indexSeg = remainingTime.length - 1;
 
-  setInterval(countDown, 1000);
+  idInterval = setInterval(countDown, 1000);
 })
