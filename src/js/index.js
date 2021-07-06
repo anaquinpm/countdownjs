e24b19221820f880637e7017a428c6af4095bfb9
Pablo Martín Anaquín <anaquinpm@gmail.com>
2021-07-05 21:38:53 -0300

    chore: counter in HTML

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,8 @@
+window.$ = document.querySelector.bind(document);
+
 document.addEventListener('DOMContentLoaded', () => {
-  // TODO: input and save in to storage the event day
+  const timeDom = [$('.main__day'), $('.main__hour'), $('.main__min'), $('.main__sec')];
+
   const trunc = (x) => Math.trunc(diffDate / x);
   const sustMillis = (s) => { diffDate = diffDate % s };
   const positiveNumber = (x) => x > 0;
@@ -14,30 +17,30 @@ document.addEventListener('DOMContentLoaded', () => {
     let gratherIndex = remainingTime.findIndex(positiveNumber);
     if (gratherIndex != -1) {
       for (index -= 1; remainingTime[index] === 0; index--) {
-        remainingTime[index] = 59;
+        timeDom[index].innerHTML = remainingTime[index] = 59;
       }
-      remainingTime[index] -= 1;
-      remainingTime[indexSeg] = 59;
+      timeDom[index].innerHTML = remainingTime[index] -= 1;
+      timeDom[indexSeg].innerHTML = remainingTime[indexSeg] = 59;
     } else {
       clearInterval(idInterval);
     }
   }
   
   function countDown() {
-    (remainingTime[indexSeg] > 0) ? remainingTime[indexSeg] -= 1 : sustNextDigit(indexSeg);
-    
-    console.log(`${remainingTime[0]} ${remainingTime[1]} ${remainingTime[2]} ${remainingTime[3]}`);
+    (remainingTime[indexSeg] > 0) ? timeDom[indexSeg].innerHTML = remainingTime[indexSeg] -= 1 : sustNextDigit(indexSeg);
+    timeDom.forEach((digit,i)=>digit.innerHTML = remainingTime[i]);
   }
   
+  // TODO: input and save in to storage the event day
   // Seteo manual de tiempo para el contador
   // !Date.UTC para poder cargar con numeros el día del evento -> mes del 0-11
   // const eventDay = new Date(Date.UTC(2021, 6, 6, 0, 58, 10));  //para seteo manual
 
   // * Pruebas de tiempo
   const eventDay = new Date();
-  // eventDay.setTime(eventDay.getTime() + 8.64e7*14 + 1000*3)  // 14d:0h:0m:3s
+  eventDay.setTime(eventDay.getTime() + 8.64e7*14 + 1000*3)  // 14d:0h:0m:3s
   // eventDay.setTime(eventDay.getTime() + 3.6e6*1 + 1000*3)    // 1h:0m:3s
-  eventDay.setTime(eventDay.getTime() + 1000 * 3)            // 3seg
+  // eventDay.setTime(eventDay.getTime() + 1000 * 3)            // 3seg
 
   const now = new Date();
 
