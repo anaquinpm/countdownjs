12a125c119d78806acddc70ddc7d9af6d840515b
Pablo Martín Anaquín <anaquinpm@gmail.com>
2021-07-13 02:05:45 -0300

    fix: package.json configuration and index.js comments

--- src/js/index.js
+++ src/js/index.js
@@ -31,29 +31,22 @@ document.addEventListener('DOMContentLoaded', () => {
     timeDom.forEach((digit, i) =>
       (remainingTime[i] > 9) ? digit.innerHTML = remainingTime[i] : digit.innerHTML = '0' + remainingTime[i]);
   }
-  //
-  // TODO: crear modal para cargar los datos o resetear el existente -> Borrar pruebas de tiempo
-  // TODO: input and save in to storage the event day -> LocalStorage
 
-  // Cargado de la fecha mediante prompt
-  // const promptFecha = prompt('Ingrese la fecha del evento: AAAA-MM-DD', 'AAAA-MM-DD').split('-');
-  // const promptHora = prompt('Ingrese el horarios del evento: HH-MM', 'HH-MM').split('-');
-  // let datePrompt = [...promptFecha, ... promptHora];
-  // // !Date.UTC para poder cargar con numeros el día del evento -> mes del 0-11
-  // datePrompt[1]--;
-  // const eventDay = new Date(...datePrompt);
+  // Parameters in milliseconds
+  const dayMill = 8.64e7;
+  const hourMill = 3.6e6;
+  const minMill = 6e4;
+  const secMill = 1000;
 
-
-
-  // * Pruebas de tiempo
+  // * Set Coundown
   const eventDay = new Date();
-  eventDay.setTime(eventDay.getTime() + 8.64e7 * 14 + 1000 * 3)  // 14d:0h:0m:3s
-  // eventDay.setTime(eventDay.getTime() + 3.6e6*1 + 1000*3)    // 1h:0m:3s
-  // eventDay.setTime(eventDay.getTime() + 1000 * 3)            // 3seg
+  eventDay.setTime(eventDay.getTime() + dayMill * 14 + secMill * 3)  // 14d:0h:0m:3s
+  // eventDay.setTime(eventDay.getTime() + hourMill + secMill * 3)    // 1h:0m:3s
+  // eventDay.setTime(eventDay.getTime() + secMill * 3)            // 3seg
 
   const now = new Date();
   let diffDate = eventDay - now;
-  let remainingTime = [timer(8.64e7), timer(3.6e6), timer(6e4), timer(1000)]
+  let remainingTime = [timer(dayMill), timer(hourMill), timer(minMill), timer(secMill)]
   const indexSeg = remainingTime.length - 1;
 
   const idInterval = setInterval(countDown, 1000);
